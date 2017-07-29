ca3e4bdff0edd970ebac9a540e9bb97bfbb11723
sa947 <sa947@njit.edu>
2017-07-29 13:36:07 -0400

    Add files via upload

---
+++ view/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>My Guitar Shop</title>
+    <link rel="stylesheet" type="text/css"
+          href="/ex_solutions/ch15_ex2_sol/main.css">
+</head>
+
+
+<body>
+<header><h1>My Guitar Shop</h1></header>
